432bee0c7e91755f21727644ada21a9243f437a4
Paul M. Heider <paul.heider@gmail.com>
2017-03-09 15:03:00 -0500

    five button app to measure dude/non-dude vs. facilitator/non-f speech
    See absolute time and percentage of time that these four classes
    of folks speak.  A pause button lets you stop the timer.  Otherwise,
    each button is tap to start (and stop the other categories from
    accruing time).

---
+++ README.md
@@ -0,0 +1,10 @@
+You Done, Bro?
+==============
+
+An app for monitoring gender and status biases in conversational turn-taking patterns.
+
+See Also
+--------
+
+- [Are men talking too much?](http://arementalkingtoomuch.com/)
+- [WomanInterrupted](http://www.womaninterruptedapp.com/)
---
+++ main.py
@@ -0,0 +1,127 @@
+from __future__ import print_function
+
+import time
+import datetime
+
+import kivy
+
+kivy.require('1.9.1')
+
+from kivy.app import App
+from kivy.lang import Builder
+
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.clock import Clock
+
+from kivy.event import EventDispatcher
+from kivy.properties import ObjectProperty
+
+class StopWatch(EventDispatcher):
+    
+    def get_running(self):
+        return self.__start_time != 0
+    
+    def get_elapsed(self):
+        if( self.__running ):
+            return self.__offset + ( time.time() - self.__start_time )
+        
+        return self.__offset
+    
+    def start(self):
+        """Starts the stopwatch."""
+        
+        self.__start_time = time.time()
+        self.__running = True
+    
+    def stop(self):
+        """Stops the stopwatch and returns the elapsed time."""
+
+        if( self.__running ):
+            self.__offset += time.time() - self.__start_time
+            self.__start_time = 0
+            self.__running = False
+        return self.get_elapsed()
+    
+    def __init__(self):
+        self.__offset = 0
+        self.__start_time = 0
+        self.__running = False
+    
+    
+    def __repr__(self):
+        hours, remainder = divmod( int( self.get_elapsed() ) , 3600 )
+        minutes, seconds = divmod( remainder , 60 )
+        return '{:02d}:{:02d}:{:02d}'.format( hours , minutes , seconds )
+
+
+class RootWidget(FloatLayout):
+    fac_dude_watch = ObjectProperty(None)
+    fac_nd_watch = ObjectProperty(None)
+    nf_dude_watch = ObjectProperty(None)
+    nf_nd_watch = ObjectProperty(None)
+    
+    def tap(self, button):
+        if( button == 'pause' ):
+            self.fac_dude_watch.stop()
+            self.fac_nd_watch.stop()
+            self.nf_dude_watch.stop()
+            self.nf_nd_watch.stop()
+        elif( button == 'fac_dude' and
+              not self.fac_dude_watch.get_running() ):
+            self.fac_dude_watch.start()
+            self.fac_nd_watch.stop()
+            self.nf_dude_watch.stop()
+            self.nf_nd_watch.stop()
+        elif( button == 'fac_nd' and
+              not self.fac_nd_watch.get_running() ):
+            self.fac_dude_watch.stop()
+            self.fac_nd_watch.start()
+            self.nf_dude_watch.stop()
+            self.nf_nd_watch.stop()
+        elif( button == 'nf_dude' and
+              not self.nf_dude_watch.get_running() ):
+            self.fac_dude_watch.stop()
+            self.fac_nd_watch.stop()
+            self.nf_dude_watch.start()
+            self.nf_nd_watch.stop()
+        elif( button == 'nf_nd' and
+              not self.nf_nd_watch.get_running() ):
+            self.fac_dude_watch.stop()
+            self.fac_nd_watch.stop()
+            self.nf_dude_watch.stop()
+            self.nf_nd_watch.start()
+            
+    def update( self, *args ):
+        four_way_timers.ids[ 'fac_dude_time' ].text = '{}'.format( self.fac_dude_watch )
+        four_way_timers.ids[ 'fac_nd_time' ].text = '{}'.format( self.fac_nd_watch )
+        four_way_timers.ids[ 'nf_dude_time' ].text = '{}'.format( self.nf_dude_watch )
+        four_way_timers.ids[ 'nf_nd_time' ].text = '{}'.format( self.nf_nd_watch )
+        total_time = self.fac_dude_watch.get_elapsed() + \
+                     self.fac_nd_watch.get_elapsed() + \
+                     self.nf_dude_watch.get_elapsed() + \
+                     self.nf_nd_watch.get_elapsed()
+        if( total_time > 0 ):
+            four_way_timers.ids[ 'fac_dude_pct' ].text = '{:0.02f}%'.format( 100 * self.fac_dude_watch.get_elapsed() / total_time )
+            four_way_timers.ids[ 'fac_nd_pct' ].text = '{:0.02f}%'.format( 100 * self.fac_nd_watch.get_elapsed() / total_time )
+            four_way_timers.ids[ 'nf_dude_pct' ].text = '{:0.02f}%'.format( 100 * self.nf_dude_watch.get_elapsed() / total_time )
+            four_way_timers.ids[ 'nf_nd_pct' ].text = '{:0.02f}%'.format( 100 * self.nf_nd_watch.get_elapsed() / total_time )
+    
+    def __init__(self, **kwargs):
+        super(RootWidget, self).__init__(**kwargs)
+        self.fac_dude_watch = StopWatch()
+        self.fac_nd_watch = StopWatch()
+        self.nf_dude_watch = StopWatch()
+        self.nf_nd_watch = StopWatch()
+        Clock.schedule_interval( self.update , 1 )
+
+four_way_timers = Builder.load_file( "main.kv" )
+
+class YouDoneBroApp(App):
+    
+    def build(self):
+        return four_way_timers
+
+if __name__ == '__main__':
+    YouDoneBroApp().run()
